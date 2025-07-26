f99cec6ced716c8be9e51c9cc621848b7a7812b2
mithunurs17 <mithunursmithun@gamil.com>
2025-07-26 18:22:13 +0530

    changes

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: D:\KALIKA\Kalika\app\page.tsx
+// File: C:\Users\Mithun Raj Urs TV\Desktop\Kalika\app\page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
